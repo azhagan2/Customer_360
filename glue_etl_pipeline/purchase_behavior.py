7bf026ea1bb5816c682f68f3a6518a2acfad74db
velmuruganms12 ms <velmuruganms12@gmail.com>
2025-05-26 09:06:48 +0530

    added test cases

--- glue_etl_pipeline/purchase_behavior.py
+++ glue_etl_pipeline/purchase_behavior.py
@@ -13,26 +13,20 @@ from glue_etl_pipeline.glue_config import USER_MYSQL_URL,ORDER_MYSQL_URL,PRODUCT
 # Parse job arguments
 args = getResolvedOptions(sys.argv, ["JOB_NAME", "S3_TARGET_PATH"])
 
+
 # Initialize Spark and Glue Context
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+s3_output_path =args['S3_TARGET_PATH'] +args["JOB_NAME"]
 
-
-
-def get_spark():
-    from pyspark.context import SparkContext
-    from awsglue.context import GlueContext
-    sc = SparkContext()
-    glueContext = GlueContext(sc)
-    job = Job(glueContext)
-    job.init(args["JOB_NAME"], args)
-    s3_output_path =args['S3_TARGET_PATH'] +args["JOB_NAME"]
-    glueContext = GlueContext(sc)
-    return glueContext.spark_session
 # Initialize Logger
 logger = get_glue_logger()
 
 def run_etl():
     try:
-        spark=get_spark()
         print("Staring ETL Job " +args["JOB_NAME"])
 
         print("starting Puchase Behaviour   ")
