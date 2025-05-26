a01ee32649fa32acd6e958c3bec46c1b3a68d550
velmuruganms12 ms <velmuruganms12@gmail.com>
2025-05-26 15:00:00 +0530

    added test case

--- glue_etl_pipeline/purchase_behavior.py
+++ glue_etl_pipeline/purchase_behavior.py
@@ -1,125 +1,46 @@
+# jobs/purchase_behavior_etl.py
+
 import sys
-import boto3
 from pyspark.context import SparkContext
-from pyspark.sql import SparkSession
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.utils import getResolvedOptions
-from pyspark.sql import functions as F
-from pyspark.sql.window import Window
-from glue_etl_pipeline.utils import get_glue_logger,read_from_rds,write_to_s3
-from glue_etl_pipeline.glue_config import USER_MYSQL_URL,ORDER_MYSQL_URL,PRODUCT_MYSQL_URL
 
-# Parse job arguments
+from glue_etl_pipeline.transformations.customer_ranking import transform_top_customers_sql
+from glue_etl_pipeline.utils import get_glue_logger, write_to_s3
+
 args = getResolvedOptions(sys.argv, ["JOB_NAME", "S3_TARGET_PATH"])
 
-
-# Initialize Spark and Glue Context
+# Initialize Glue context
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
+
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
-s3_output_path =args['S3_TARGET_PATH'] +args["JOB_NAME"]
-
-# Initialize Logger
 logger = get_glue_logger()
+s3_output_path = args["S3_TARGET_PATH"] + args["JOB_NAME"]
 
-def run_etl():
-    try:
-        print("Staring ETL Job " +args["JOB_NAME"])
+try:
+    logger.info(f"Running ETL Job: {args['JOB_NAME']}")
 
-        print("starting Puchase Behaviour   ")
-        print("S3 Target Path: " + s3_output_path)
-        print("  starting transformation")
+    # Read from Glue catalog
+    customer_df = spark.read.table("bronze_db.customers_raw")
+    order_df = spark.read.table("bronze_db.orders_raw")
+    
+    customer_df.createOrReplaceTempView("customers")
+    order_df.createOrReplaceTempView("orders")
 
-        customer_df = spark.read.table("bronze_db.customers_raw")
-        order_df = spark.read.table("bronze_db.orders_raw")
-        customer_df.createOrReplaceTempView("customers")
-        order_df.createOrReplaceTempView("orders")
+    # Transform
+    top_customers = transform_top_customers_sql(spark)
 
-        customer_df.show()
-        
-        #common tranformation 
-        top_customers=transform_sql()
-        print("Running  SQL Query  for top customers")
+    # Write output
+    write_to_s3(top_customers, s3_output_path)
 
+    logger.info("ETL Job completed successfully")
 
-        top_customers.show()
-        print(top_customers.count())
-        #top_customers=transform_dataframe(order_df,customer_df)
+except Exception as e:
+    logger.error(f"ETL Job Failed: {e}")
+    raise e
 
-        
-        write_to_s3(top_customers,s3_output_path)
-
-        print("ETL Job Completed Successfully")
-
-    except Exception as e:
-        print(f"ETL Job Failed: {str(e)}")
-        raise e
-    job.commit()
-
-
-
-def transform_sql():
-            # Run Spark SQL Query
-        top_customers = spark.sql("""
-             WITH customer_spending AS (
-        SELECT
-            o.customer_id,
-            SUM(o.total_amount) AS total_spent,
-            COUNT(o.order_id) AS total_orders,
-            MAX(o.order_date) AS last_purchase_date
-        FROM orders o
-        WHERE o.order_date >= date_add(current_date(), -365)  -- Last 1 year
-        GROUP BY o.customer_id
-    ),
-    customer_ranking AS (
-        SELECT
-            c.country,
-            c.customer_id,
-            CONCAT(c.first_name, ' ', c.last_name) AS full_name,
-            c.email,
-            cs.total_spent,
-            cs.total_orders,
-            cs.last_purchase_date,
-            RANK() OVER (PARTITION BY c.country ORDER BY cs.total_spent DESC) AS spending_rank
-        FROM customer_spending cs
-        JOIN customers c ON cs.customer_id = c.customer_id
-    )
-    SELECT * FROM customer_ranking WHERE spending_rank <= 10 and country like 'United %';
-        """)
-        return top_customers
-
-
-def transform_dataframe(order_df,customer_df):
-    # Filter last 1 year of data
-    one_year_ago = F.date_add(F.current_date(), -365)
-    filtered_orders = order_df.filter(F.col("order_date") >= one_year_ago)
-
-    # Aggregate customer spending
-    customer_spending = (
-        filtered_orders.groupBy("customer_id")
-        .agg(
-            F.sum("total_amount").alias("total_spent"),
-            F.count("order_id").alias("total_orders"),
-            F.max("order_date").alias("last_purchase_date")
-        )
-    )
-
-    # Join with customers table
-    customer_data = customer_spending.join(customer_df, "customer_id")
-
-    # Define window specification for ranking
-    window_spec = Window.partitionBy("country").orderBy(F.desc("total_spent"))
-
-    # Add ranking column
-    customer_ranking = customer_data.withColumn("spending_rank", F.rank().over(window_spec))
-
-    # Filter top 100 customers
-    top_customers = customer_ranking.filter( (F.col("spending_rank") <= 10) &  (F.col("country").like("United %")))
-
-    # Show results
-    top_customers.select("country","customer_id","first_name","email","total_spent","total_orders","spending_rank").show(20)
-
-    return top_customers
+job.commit()
