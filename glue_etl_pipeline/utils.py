21f09012d71b86845d4148fa5e6dc31ef4f31d8c
velmuruganms12 ms <velmuruganms12@gmail.com>
2025-07-27 19:45:06 +0530

    modified etl files

--- glue_etl_pipeline/utils.py
+++ glue_etl_pipeline/utils.py
@@ -49,10 +49,11 @@ def write_audit_log(spark, job_name, status, customer_count, start_time, end_tim
     audit_df = spark.createDataFrame([
         (job_name, status, customer_count, start_time, end_time, datetime.now())
     ], ["job_name", "status", "record_count", "start_time", "end_time", "log_ts"])
-    audit_df.write.mode("append").format("delta").saveAsTable("enterprise_db.audit_log")
+    audit_df.write.mode("append").insertInto("enterprise_db.audit_log")
 
 def update_control_table(spark, job_name, status):
     control_df = spark.createDataFrame([
         (job_name, status, datetime.now())
     ], ["job_name", "run_status", "run_ts"])
-    control_df.write.mode("append").format("delta").saveAsTable("enterprise_db.control_table")
+    #control_df.write.mode("append").format("delta").saveAsTable("enterprise_db.control_table")
+    control_df.write.mode("append").insertInto("enterprise_db.control_table")
