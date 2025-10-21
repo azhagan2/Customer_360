863e16c680d22710280c5b09e3c38d634e19347d
velmurugan <velmuruganms12@gmail.com>
2025-10-21 08:27:10 +0530

    added new changes for preprocessing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="customer_analytics",  # Replace with your package name
-    version="0.2.0",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "boto3",
